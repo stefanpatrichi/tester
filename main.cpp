e4a3495a3b330d1d8bfbe121c51cde7b430185f1
Patrichi Ștefan <stefanpatrichi07@gmail.com>
2022-06-21 18:02:02 +0300

    add boost program options

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
+#include <iostream>
+#include <string>
+
+#include <boost/program_options.hpp>
+
 #define FASTRW_IMPLEMENTATION
 #include "fastrw.hpp"
 
-int main() {
+int main(int argc, const char *argv[]) {
+    namespace po = boost::program_options;
+    
+    std::string usage("./tester --source <cpp-source-file> --input <input-file> "
+        "--output <output-file> --test <path/to/tests/folder>");
 
+    try {
+        po::options_description desc("USAGE: " + usage + "\nAllowed options");
+        desc.add_options()
+            ("help", "Produce this help message");
+
+        po::variables_map vm;
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+        po::notify(vm);    
+    } catch(const std::exception& e) {
+        std::cerr << e.what() << '\n';
+    }
+    
     return 0;
 }
